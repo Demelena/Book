4b504f26e53a6ad0323c06e22093b12aac55aa99
Demelena <demelena@gmail.com>
2018-10-18 23:59:24 -0700

    commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello, World");
+        System.out.print("Hi, ");
+        System.out.print(args[0]);
+        System.out.println(". How are you?");
+        System.out.println("Hi, my dear " + args[0] + ". How are you?");
+    }   
+}
