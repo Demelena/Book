53f08ea93614249fb23906ae03c28a058dd66f14
Demelena <demelena@gmail.com>
2018-10-29 22:01:16 -0700

    Easy Fix

--- Quadratic.java
+++ Quadratic.java
@@ -6,7 +6,7 @@ double b = Double.parseDouble(args[0]);
 double c = Double.parseDouble(args[1]);
 double discriminant = b*b - 4.0*c;
 double d = Math.sqrt(discriminant);
-System.out.printin((-b + d) / 2.0);
-System.out.printin((-b - d) / 2.0);
+System.out.println((-b + d) / 2.0);
+System.out.println((-b - d) / 2.0);
 }
 }
