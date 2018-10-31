b864f4a61f4ad7dae3ef8814ea4f1989ac9ef42b
Demelena <demelena@gmail.com>
2018-10-30 18:00:26 -0700

    adding Buil in types of data exercises

---
+++ BuiltInTypesOfData.java
@@ -0,0 +1,26 @@
+public class BuiltInTypesOfData
+// Max value
+{
+public static void main(String[] args)
+{
+ int a = Integer.MAX_VALUE;
+double b = 3.14159;
+
+ System.out.println(a);
+ System.out.println(a+1);
+ System.out.println(2-a);
+ System.out.println(-2-a);
+ System.out.println(2*a);
+ System.out.println(4*a);
+
+ System.out.println(b);
+ System.out.println(b+1);
+ System.out.println(8/(int) b);
+ System.out.println(8/b);
+ System.out.println((int) (8/b));
+}
+
+
+
+
+}
