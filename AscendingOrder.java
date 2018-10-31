aa26ab43d23a7572cb4fd8a5d21ce3292fe1d640
Demelena <demelena@gmail.com>
2018-10-31 12:52:32 -0700

    adding printing numbers in ascending order

---
+++ AscendingOrder.java
@@ -0,0 +1,18 @@
+public class AscendingOrder
+// This program takes three integer command-line arguments and prints them in ascending order.
+{
+    public static void main(String[] args)
+    {
+        int a = Integer.parseInt( args[0] );
+        int b = Integer.parseInt( args[1] );
+        int c = Integer.parseInt( args[2] );
+        int x = Math.min(a, b);
+        int min = Math.min(x, c);
+        int z = Math.max(a, b);
+        int max = Math.max(z, c);
+        int mid = a + b + c - min - max;
+        System.out.println(min);
+        System.out.println(mid);
+        System.out.println(max);  
+    }
+}
